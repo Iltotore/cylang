9f5fee6da47ffe6dc11b9cc5c6d4afa73775050c
Iltotore <rafbodaha@gmail.com>
2022-02-25 11:20:34 +0100

    feat: Add intuitive EvaluationError message

--- main/src/io/github/iltotore/cylang/Cursor.scala
+++ main/src/io/github/iltotore/cylang/Cursor.scala
@@ -1,3 +1,6 @@
 package io.github.iltotore.cylang
 
-case class Cursor(function: String, position: Position)
+case class Cursor(function: String, position: Position) {
+
+  lazy val trace: String = s"Vers la ligne ${position.line}, colonne ${position.column}, dans $function"
+}
--- main/src/io/github/iltotore/cylang/eval/EvaluationError.scala
+++ main/src/io/github/iltotore/cylang/eval/EvaluationError.scala
@@ -3,14 +3,31 @@ package io.github.iltotore.cylang.eval
 import io.github.iltotore.cylang.ast.Value
 import io.github.iltotore.cylang.{CYType, Context, Cursor}
 
-case class EvaluationError(message: String, stack: List[Cursor]) extends Exception(message)
+case class EvaluationError(message: String, stack: List[Cursor]) extends Exception(message) {
+
+  lazy val fullMessage: String = {
+
+    if (stack.isEmpty) s"Erreur: $message"
+    else
+      s"""Erreur: $message
+         |
+         |${stack.head.trace}
+         |
+         |${stack.head.position.content}
+         |
+         |
+         |${stack.tail.map(_.trace).mkString("\n")}""".stripMargin
+  }
+
+  override def toString: String = fullMessage
+}
 
 object EvaluationError {
-  
+
   def apply(message: String)(using context: Context): EvaluationError = EvaluationError(message, context.stack)
-  
+
   def impossible(stack: List[Cursor]): EvaluationError = EvaluationError("This is a language bug!", stack)
-  
+
   def impossible(using context: Context): EvaluationError = impossible(context.stack)
 
   def typeMismatch(got: Value)(using Context): EvaluationError = EvaluationError(s"Type incompatible pour la valeur $got")
